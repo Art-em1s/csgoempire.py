49ac0ec1d120b10a0c310abfe9b016f64823d2ab
Artemis <Artemis@w1z0.xyz>
2022-02-08 07:30:38 +0000

    Force metadata to update every 6h

--- metadata.py
+++ metadata.py
@@ -2,9 +2,12 @@ from ._types import Meta, InvalidApiKey, User, RequestError, ExceedsRatelimit
 import requests
 
 from os import environ as env
+from time import time
 
 
 class Metadata():
+    metadata_update_period = 60*60*6 #6 hours
+    
     def __init__(self):
         self.is_multiuser = False #todo implement multiuser for account linking
         self.user: User = None #instance of user
@@ -15,10 +18,11 @@ class Metadata():
         self.api_key = env['api_key']
         self.api_base_url = env['api_base_url']
         self.headers = {'Authorization': f'Bearer {self.api_key}','Content-Type': 'application/json'}
+        self.last_update = None
         
         
         #set metadata on initalisation
-        self.get_metadata()
+        self.set_metadata()
         
     def set_metadata(self):
         url = self.api_base_url + "metadata/socket"
@@ -30,6 +34,7 @@ class Metadata():
         if status == 200:
             self.metadata = Meta(response)
             self.user = User(self.metadata.user)
+            self.last_update = time()
         else:
             if status == 401:
                 raise InvalidApiKey()
@@ -39,34 +44,34 @@ class Metadata():
                 raise RequestError(response)
         
     def get_metadata(self):
-        if self.metadata is None:
+        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
         return self.metadata    
         
     def get_user(self):
-        if self.metadata is None:
+        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
             
         return self.metadata.user
                 
     def get_socket_token(self):
-        if self.metadata is None:
+        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
         return self.metadata.socket_token
         
     def get_socket_signature(self):
-        if self.metadata is None:
+        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
         return self.metadata.socket_signature
     
     def get_user_id(self):
-        if self.metadata is None:
+        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
             
         return self.user.id
         
     def get_identify(self):
-        self.set_metadata() #force get idenitfy to update metadata
+        self.set_metadata() #force get identify to update metadata
             
         auth = {
             "uid": self.get_user_id(),
