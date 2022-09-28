fcb48d818cbc370355d4fe287b94a32085729084
Artemis <Artemis@w1z0.xyz>
2022-09-28 15:59:56 +0100

    Fix: prevent updates where the server disallows

--- deposits.py
+++ deposits.py
@@ -14,6 +14,7 @@ class Deposits(dict):
         self.api_base_url = env['api_base_url']
         self.headers = {'Authorization': f'Bearer {self.api_key}','Content-Type': 'application/json'}
         self.deposit = Deposit()
+        self.can_refresh = False
         
     def get_active_deposits(self):
         url = self.api_base_url+"trading/user/trades"
@@ -38,6 +39,10 @@ class Deposits(dict):
                 raise RequestError(f"Deposits:get_active_deposits: {response['message']}")
             
     def get_inventory(self, force_refresh=False):
+        
+        if force_refresh and not self.can_refresh: #if refresh is requested but server hasn't allowed refresh
+            force_refresh = False
+        
         url = self.api_base_url+"trading/user/inventory?update="+str(force_refresh)
         response = requests.get(url, headers=self.headers)
         
@@ -48,6 +53,7 @@ class Deposits(dict):
         app = inventory.append
         
         if status == 200:
+            self.can_refresh = response['allowUpdate']
             for item in response['data']:
                 if "invalid" in item or item['market_value'] < 0 or item['tradable'] == False:
                     continue
