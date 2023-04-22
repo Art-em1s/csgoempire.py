6372db8e5c8c5290f077ae1a8f84564d2721cafa
Artemis <Artemis@w1z0.xyz>
2023-04-22 13:41:05 +0100

    refactor: Update Metadata class to use private attributes and handle_error function

--- metadata.py
+++ metadata.py
@@ -1,4 +1,4 @@
-from ._types import Meta, InvalidApiKey, User, RequestError, ExceedsRatelimit
+from ._types import Meta, User, handle_error
 import requests
 
 from time import time
@@ -12,15 +12,19 @@ class Metadata():
         self.api_base_url = api_base_url
         # self.is_multiuser = False  # todo implement multiuser for account linking
         # self.users: list = None  # list of users
-        self.user: User = None  # instance of user
-        self.metadata = None
-        self.socket_token = None
-        self.socket_signature = None
+        self._user = None  # instance of a user
+        self._metadata = None
+        self._socket_token = None
+        self._socket_signature = None
         self.headers = {'Authorization': f'Bearer {self.api_key}', 'Content-Type': 'application/json'}
         self.last_update = None
 
         # set metadata on initalisation
         self.set_metadata()
+    
+    # return metadata as a string
+    def __repr__(self):
+        return str(self._metadata)
 
     def set_metadata(self):
         url = self.api_base_url + "metadata/socket"
@@ -30,35 +34,35 @@ class Metadata():
         response = response.json()
 
         if status == 200:
-            self.metadata = Meta(response)
-            self.user = User(self.metadata.user)
+            self._metadata = Meta(response)
+            self._user = User(self._metadata.user)
             self.last_update = time()
         else:
-            self.handle_error(status, response, "set_metadata")
+            handle_error(status, response, "Meta", "set_metadata")
 
     def ensure_metadata_updated(self):
-        if self.metadata is None or time() - self.last_update > self.metadata_update_period:
+        if self._metadata is None or time() - self.last_update > self.metadata_update_period:
             self.set_metadata()
 
     @property
     def user(self):
         self.ensure_metadata_updated()
-        return self.metadata.user
+        return self._user
 
     @property
     def socket_token(self):
         self.ensure_metadata_updated()
-        return self.metadata.socket_token
+        return self._metadata.socket_token
 
     @property
     def socket_signature(self):
         self.ensure_metadata_updated()
-        return self.metadata.socket_signature
+        return self._metadata.socket_signature
 
     @property
     def user_id(self):
         self.ensure_metadata_updated()
-        return self.user.id
+        return self._user.id
 
     def get_identify(self):
         self.set_metadata()  # force get identify to update metadata
@@ -75,17 +79,10 @@ class Metadata():
     @property
     def balance(self):
         self.ensure_metadata_updated()
-        return self.user.balance
+        return self._user.balance
 
     @property
     def steam_api_key(self):
         self.ensure_metadata_updated()
-        return self.user.steam_api_key
-
-    def handle_error(self, status, response, function_name):
-        exception_mapping = {
-            401: InvalidApiKey,
-            429: ExceedsRatelimit
-        }
-        exception_class = exception_mapping.get(status, RequestError)
-        raise exception_class(f"Meta:{function_name}:{status}: {response['message']}")
+        return self._user.steam_api_key
+    
